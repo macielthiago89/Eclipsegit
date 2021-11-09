389e4466927670a38a72c83ec1d69bc0f8b4e1f8
macie <macie@192.168.0.190>
2021-11-09 17:51:05 -0300

    teste2

--- EclipseGitHub/src/Aulagit65/Pessoa.java
+++ EclipseGitHub/src/Aulagit65/Pessoa.java
@@ -2,5 +2,21 @@ package Aulagit65;
 
 public class Pessoa {
 	private String name;
+	
+	private int idade;
+	
+	public Pessoa() {
+		
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	
 }
